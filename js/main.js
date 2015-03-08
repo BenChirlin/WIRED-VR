bd74c054aed2b4a3264639f2ef9d65bd64b79d6e
Benjamin Chirlin <Benjamin_Chirlin@wired.com>
2015-03-07 18:51:46 -0800

    Making some key values into vars

--- js/main.js
+++ js/main.js
@@ -39,10 +39,14 @@ var geometry = new THREE.BoxGeometry( 2, 2, 2 );
 
 // Number of objects to place per row
 var perRow = 12;
+// Total objects
+var totalObjects = 120;
+// Total rows
+var totalRows = Math.ceil( totalObjects / perRow );
 // Get angle between objects
 var angle = ( 2 * Math.PI ) / perRow;
 
-for ( var i = 0; i < 120; i++ ) {
+for ( var i = 0; i < totalObjects; i++ ) {
     // Get objects row
     var row = Math.floor( i / perRow );
     // Calculate year based on row and index
@@ -62,7 +66,7 @@ for ( var i = 0; i < 120; i++ ) {
     object.position.x = Math.cos( angle * ( i % perRow ) ) * 6;
     object.position.z = Math.sin( angle * ( i % perRow ) ) * 6;
     // Offset Y so camera is staring at middle row
-    object.position.y = ( row - 6 ) * 4;
+    object.position.y = ( ( row - totalRows ) * 4 ) + 4;
 
     // Compensate for position and rotate back towards origin
     object.rotation.y = ( Math.PI / 2 ) - ( angle * ( i % perRow ) );
@@ -112,20 +116,28 @@ document.body.addEventListener( 'dblclick', function() {
     effect.setFullScreen( true );
 });
 
+function changeRow( offset ) {
+
+}
+
 /*
 Listen for keyboard events to zero positional sensor or enter full-screen VR mode.
 */
 function onkey(event) {
 
-if (!(event.metaKey || event.altKey || event.ctrlKey)) {
+    if (!(event.metaKey || event.altKey || event.ctrlKey)) {
         event.preventDefault();
     }
 
-if (event.charCode == 'z'.charCodeAt(0)) { // z
-    controls.zeroSensor();
-} else if (event.charCode == 'f'.charCodeAt(0)) { // f
-    effect.setFullScreen( true );
-}
+    if (event.charCode == 'z'.charCodeAt(0)) { // z
+        controls.zeroSensor();
+    } else if (event.charCode == 'f'.charCodeAt(0)) { // f
+        effect.setFullScreen( true );
+    } else if (event.charCode == 'w'.charCodeAt(0)) { // w
+        changeRow( 1 );
+    } else if (event.charCode == 's'.charCodeAt(0)) { // s
+        changeRow( 2 );
+    }
 };
 
 window.addEventListener("keypress", onkey, true);
